fc7aa6334c1f5b6a53cf1d82c8e292bca0af228f
pricklynut <pricklynut@gmail.com>
2017-08-30 23:54:01 +0300

    fixtures data

--- config/schema.sql
+++ config/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE tasks (
   image VARCHAR(255),
   status ENUM('finished', 'new') NOT NULL DEFAULT 'new'
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO tasks (username, email, task) VALUES ('user1', 'user1@example.com', 'Описание первой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user2', 'user2@example.com', 'Описание второй задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user1', 'user1@example.com', 'Описание третьей задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user3', 'user3@example.com', 'Описание четвертой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user2', 'user2@example.com', 'Описание пятой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user1', 'user1@example.com', 'Описание шестой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user1', 'user1@example.com', 'Описание седьмой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user3', 'user3@example.com', 'Описание восьмой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user2', 'user2@example.com', 'Описание девятой задачи');
+INSERT INTO tasks (username, email, task) VALUES ('user1', 'user1@example.com', 'Описание десятой задачи');
