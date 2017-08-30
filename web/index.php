854032d445dd72bfb3c05857a8294cf5d45b9a9c
pricklynut <pricklynut@gmail.com>
2017-08-30 19:12:07 +0300

    fc autoload, run app

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+define('ROOT_DIR', dirname(dirname(__FILE__)));
+
+spl_autoload_register(function ($class) {
+    $srcDir = ROOT_DIR . DIRECTORY_SEPARATOR . 'src';
+
+    $namespaceParts = explode('\\', $class);
+    if (count($namespaceParts) > 1) {
+        unset($namespaceParts[0]);
+    }
+
+    include_once $srcDir . DIRECTORY_SEPARATOR . join('/', $namespaceParts) . '.php';
+});
+
+$config = require ROOT_DIR . DIRECTORY_SEPARATOR . 'config/main.php';
+
+$app = App\Application::getInstance($config);
+$app->run();
